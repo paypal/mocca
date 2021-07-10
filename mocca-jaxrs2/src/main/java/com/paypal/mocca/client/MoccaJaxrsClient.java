36cbcffec8b55f29d02f654cc5c4ab7b908020b3
Samuel Cox <crankydillo@gmail.com>
2021-07-10 14:27:11 -0500

    Update mocca-jaxrs2/src/main/java/com/paypal/mocca/client/MoccaJaxrsClient.java
    Co-authored-by: Fábio Carvalho <fabiocarvalho777@gmail.com>

--- mocca-jaxrs2/src/main/java/com/paypal/mocca/client/MoccaJaxrsClient.java
+++ mocca-jaxrs2/src/main/java/com/paypal/mocca/client/MoccaJaxrsClient.java
@@ -14,7 +14,7 @@ import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Mocca JAXRS2 client. In order to use a JAX-RS 2 client with Mocca, create a new instance of this class and pass it to
+ * Mocca JAX-RS 2 client. In order to use a JAX-RS 2 client with Mocca, create a new instance of this class and pass it to
  * Mocca builder.
  * <br>
  * See {@link com.paypal.mocca.client.MoccaClient.Builder.SyncBuilder#client(MoccaHttpClient)} for further information
