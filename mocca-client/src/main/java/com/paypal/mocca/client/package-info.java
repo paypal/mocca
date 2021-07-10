731faae6c5258846eaf7ab12dac1765b997002fb
Samuel Cox <sacox@paypal.com>
2021-07-10 08:17:23 -0500

    Host javadocs on javadoc.io #3
     #3

--- mocca-client/src/main/java/com/paypal/mocca/client/package-info.java
+++ mocca-client/src/main/java/com/paypal/mocca/client/package-info.java
@@ -1,5 +1,5 @@
 /**
- * Mocca client builder and basic configuration
+ * Mocca client builder and basic configuration.  {@link com.paypal.mocca.client.MoccaClient} is a good place to start.
  *
  * @since 0.0.1
  * @author fabiocarvalho777@gmail.com
