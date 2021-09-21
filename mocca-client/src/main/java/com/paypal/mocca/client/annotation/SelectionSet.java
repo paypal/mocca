ff75f78c9d6db44661ea3ca686133c4190ac4e84
Amandeep Singh <25630548+AmandeepSingh97@users.noreply.github.com>
2021-09-21 20:12:57 +0530

    PR suggestion
    Co-authored-by: Fábio Carvalho <fabiocarvalho777@gmail.com>

--- mocca-client/src/main/java/com/paypal/mocca/client/annotation/SelectionSet.java
+++ mocca-client/src/main/java/com/paypal/mocca/client/annotation/SelectionSet.java
@@ -70,7 +70,7 @@ public @interface SelectionSet {
      * In order to do so, specify here an array containing all fields to be ignored in the Selection Set.
      * The name of a property in an inner POJO can be specified using the outer field name followed by dot.
      * If the request type is not a POJO, or if {@link #value()} is set, then {@code ignore} will have no effect.
-     * If the property set to be ignored doesn't exist, then it has no effect, the Selection set is generated normally as
+     * If the property set to be ignored doesn't exist, then it has no effect, the Selection Set is generated normally as
      * if that ignore value had not been set.
      * If both {@link #value()} and {@code ignore} attributes are set, then the {@code ignore} value is not used by Mocca, and a warning is logged.
      *
