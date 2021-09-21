fd20451b6d59fb490f18b80117d726983be9e9f1
Amandeep Singh <25630548+AmandeepSingh97@users.noreply.github.com>
2021-09-21 20:18:45 +0530

    PR suggestion
    Co-authored-by: Fábio Carvalho <fabiocarvalho777@gmail.com>

--- mocca-client/src/main/java/com/paypal/mocca/client/MoccaSerializer.java
+++ mocca-client/src/main/java/com/paypal/mocca/client/MoccaSerializer.java
@@ -404,7 +404,7 @@ class MoccaSerializer {
      *
      * @param requestPayload the output stream object used to write the selection set, based on the other parameters
      * @param responseType the return type set in the GraphQL operation method, necessary to dynamically set the selection set
-     * @param ignoreFields The list of fields which have to be ignored in selection set generation
+     * @param ignoreFields the list of fields which have to be ignored in selection set generation
      * @throws MoccaException if a cycle is found or any error happens when writing the selection set
      */
     private void writeResponseTypeSelectionSet(final ByteArrayOutputStream requestPayload, final Type responseType, List<String> ignoreFields) {
